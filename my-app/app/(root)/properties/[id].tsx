89c3fc82377376a48410c1689026dcd2fd2d1745
markohautala <markohautala@live.se>
2025-02-04 21:54:18 +0100

    Remove index.tsx; add Explore, Profile, and Property components

---
+++ my-app/app/(root)/properties/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Poperty = () => {
+  return (
+    <View>
+      <Text>Poperty</Text>
+    </View>
+  )
+}
+
+export default Poperty;
