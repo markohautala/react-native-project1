c13a9b61f5500abc167e8e5fd79ab3de43bf04bf
markohautala <markohautala@live.se>
2025-02-11 15:10:06 +0100

    Add theme, font-styling and images

--- my-app/app/(root)/(tabs)/index.tsx
+++ my-app/app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className=" font-bold text-lg my-5">Welome to RealEstate</Text>
+      <Text className="font-bold font-rubik text-3xl my-10">Welome to RealEstate</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,6 +1,31 @@
 import { Stack } from "expo-router";
-import "./globals.css"
+import "./globals.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while fonts load
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync(); // Hide splash screen once fonts are loaded
+    }
+  }, [fontsLoaded]); // Check is the fonts are loaded as a dependency
+
+  if (!fontsLoaded) {
+    return null; // Prevent UI from rendering until fonts are loaded
+  }
+
   return <Stack />;
 }
