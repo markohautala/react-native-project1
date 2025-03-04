24562412dfd63c5363d819d550e92293280eb3a8
markohautala <markohautala@live.se>
2025-03-04 16:42:11 +0100

    Update tab layout with custom icons and adjust styling

--- my-app/app/(root)/(tabs)/_layout.tsx
+++ my-app/app/(root)/(tabs)/_layout.tsx
@@ -1,20 +1,61 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 
+import icons from "@/constants/icons";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => (
+  <View className="flex-1 mt-3 flex-col items-center">
+    <Image
+      source={icon}
+      tintColor={focused ? "#b3cde4" : "#001b2e"}
+      resizeMode="contain"
+      className="size-7"
+    />
+    <Text
+      className={`${
+        focused
+          ? "text-primary-300 font-rubik-medium"
+          : "text-black-200 font-rubik-medium"
+      } text-xs w-full marker:text-center mt-1`}
+    >
+      {title}
+    </Text>
+  </View>
+);
+
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{
-      tabBarShowLabel: false,
-      tabBarStyle: {
-        backgroundColor: "white",
-        position: "absolute",
-        borderTopColor: "#000000",
-        borderTopWidth: 1,
-        minHeight: 60,
-      },
-    }}>
-      <Text>Tabs</Text>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#b3cde4",
+          borderTopWidth: 2,
+          minHeight: 60,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home"></TabIcon>
+          ),
+        }}
+      />
     </Tabs>
   );
 };
