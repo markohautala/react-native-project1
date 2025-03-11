17759fa09ba013a97388bd3841d59d453538de85
markohautala <markohautala@live.se>
2025-03-11 21:38:58 +0100

    Refactor Index screen to include user avatar and placeholder text

--- my-app/app/(root)/(tabs)/index.tsx
+++ my-app/app/(root)/(tabs)/index.tsx
@@ -1,16 +1,20 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import images from "@/constants/images";
+import { Image, SafeAreaView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold font-rubik text-3xl my-10">Welome to Peak Properties</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row justify-between items-center mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-large font-rubik text-black-100">Hello</Text>
+              <Text className="text-large text-black-300 font-rubik-medium">Marko</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
