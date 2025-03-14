d31fef0d4aa1a6798203e905fb9b57b78468bf57
markohautala <markohautala@live.se>
2025-03-14 14:24:40 +0100

    Enhance Search component with input field and filter icon

--- my-app/components/Search.tsx
+++ my-app/components/Search.tsx
@@ -1,17 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams, usePathname } from 'expo-router'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Octicons from '@expo/vector-icons/Octicons';
+
+
 
 const Search = () => {
   // Retrieves the current pathname in the application
-  const path = usePathname();
+  // const path = usePathname();
 
   // Retrieves local search parameters, where "query" is an optional parameter
   const params = useLocalSearchParams<{ query?: string }>();
 
   // Creates a state variable "query" and a function "setSearch" to update it
   // If a "query" parameter exists, it is used as the initial value; otherwise, an empty string is set
-  const [query, setSearch] = React.useState(params.query || '');
+  const [search, setSearch] = React.useState(params.query || '');
 
   // Function that handles the search by updating the "query" state
   const handleSearch = (text: string) => {
@@ -19,8 +23,17 @@ const Search = () => {
   };
 
   return (
-    <View>
-      <Text>Search</Text>
+    <View className='flex flex-row items-center justify-between w-full px-4 mt-4 bg-white border border-gray-200 rounded-lg'>
+      <View className='flex flex-1 flex-row items-center justify-start z-50'>
+        <AntDesign name="search1" size={24} color="black" />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder='Search'
+          className='text-sm font-rubik text-black-300 ml-2 flex-1'
+        />
+        <Octicons name="filter" size={24} color="black" />
+      </View>
     </View>
   )
 }
