55ab026fc1bdc72ea16cafed426e060be5ad6f34
markohautala <markohautala@live.se>
2025-03-15 15:57:12 +0100

    Implement debounced search functionality in Search component

--- my-app/components/Search.tsx
+++ my-app/components/Search.tsx
@@ -1,8 +1,9 @@
 import { View, TextInput } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams, usePathname } from 'expo-router'
+import { router, useLocalSearchParams, usePathname } from 'expo-router'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Octicons from '@expo/vector-icons/Octicons';
+import { useDebouncedCallback } from 'use-debounce'
 
 
 
@@ -17,22 +18,30 @@ const Search = () => {
   // If a "query" parameter exists, it is used as the initial value; otherwise, an empty string is set
   const [search, setSearch] = React.useState(params.query || '');
 
+
+  // Creates a debounced callback function that updates the "query" parameter in the router
+  // The function is called with the new search text and a delay of 500ms
+  const debouncedSearch = useDebouncedCallback((text: string) => {
+    router.setParams({ query: text }), 500;
+  });
+
   // Function that handles the search by updating the "query" state
   const handleSearch = (text: string) => {
     setSearch(text);
+    debouncedSearch(text);
   };
 
   return (
-    <View className='flex flex-row items-center justify-between w-full px-4 mt-4 bg-white border border-gray-200 rounded-lg'>
+    <View className='flex flex-row items-center justify-between w-full px-4 mt-4 bg-white border border-gray-200 h-10'>
       <View className='flex flex-1 flex-row items-center justify-start z-50'>
-        <AntDesign name="search1" size={24} color="black" />
+        <AntDesign name="search1" size={20} color="black" />
         <TextInput
           value={search}
           onChangeText={handleSearch}
-          placeholder='Search'
-          className='text-sm font-rubik text-black-300 ml-2 flex-1'
+          placeholder='Search for anything'
+          className='text-m font-rubik h-10 text-black-300 ml-3 flex-1'
         />
-        <Octicons name="filter" size={24} color="black" />
+        <Octicons name="filter" size={20} color="black" />
       </View>
     </View>
   )
