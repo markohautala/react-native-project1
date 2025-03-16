facf0c2eb7773ffda55586df49cfbdba3d152481
markohautala <markohautala@live.se>
2025-03-16 15:40:33 +0100

    Add FeaturedCards and Cards components to Index screen

---
+++ my-app/components/Cards.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const FeaturedCards = () => {
+  return (
+    <View>
+      <Text>FeaturedCards</Text>
+    </View>
+  )
+}
+
+export const Cards = () => {
+  return (
+    <View>
+      <Text>Cards</Text>
+    </View>
+  )
+}
