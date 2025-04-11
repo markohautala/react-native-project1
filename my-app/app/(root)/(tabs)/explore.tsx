05b1807bff45160d057175391c55a5cd5e2b7126
markohautala <markohautala@live.se>
2025-04-11 14:44:22 +0200

    Refactor Explore component to enhance property listing

--- my-app/app/(root)/(tabs)/explore.tsx
+++ my-app/app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,84 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Octicons from "@expo/vector-icons/Octicons";
+import Search from "@/components/Search";
+import { Cards, FeaturedCards } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import { useGlobalContext } from "@/lib/global-provider";
+import { router, useLocalSearchParams } from "expo-router";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
+import NoResults from "@/components/NoResults";
 
-const Explore = () => {
+export default function Explore() {
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const {
+    data: properties,
+    refetch,
+    loading,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    },
+    skip: true,
+  });
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 20,
+    });
+  }, [params.filter, params.query]);
+
+  // Pushes to the specific property page upon clicking the card
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
-  )
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={properties}
+        renderItem={({ item }) => (
+          <Cards item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        contentContainerClassName="pb-20"
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator
+              size="large"
+              color="#000000"
+              className="flex-1 mt-20 justify-center items-center"
+              style={{ height: "100%" }}
+            />
+          ) : (
+            <NoResults />
+          )
+        }
+        ListHeaderComponent={
+          <View className="px-5">
+            <Search />
+            <View className="mt-5">
+              <Filters />
+              <Text className="text-lg font-rubik-semibold text-black-200 mt-3">Found {properties?.length} Properties</Text>
+            </View>
+          </View>
+        }
+      />
+    </SafeAreaView>
+  );
 }
-
-export default Explore
