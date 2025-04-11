b1527a34874c7e6f4e6b3ba2632470e58689e439
markohautala <markohautala@live.se>
2025-04-11 08:33:10 +0200

    Refactor Filters component for improved category handling and readability

--- my-app/components/Filters.tsx
+++ my-app/components/Filters.tsx
@@ -1,36 +1,48 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { router, useLocalSearchParams } from 'expo-router'
 import { categories } from '@/constants/data';
 
 const Filters = () => {
   const params = useLocalSearchParams<{ filter?: string }>();
-  const [selectedCategory, setFilter] = useState(params.filter || "All");
+  const [selectedCategory, setSelectedCategory] = useState(params.filter || "All");
 
-  const handleCategory = (category: string) => {
+  const handleCategoryClick = (category: string) => {
     if (selectedCategory === category) {
-      setFilter("All");
-      router.setParams({ filter: "All" });
+      setSelectedCategory("");
+      router.setParams({ filter: "" });
       return;
     }
 
-    setFilter(category);
+    setSelectedCategory(category);
     router.setParams({ filter: category });
   };
 
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt-3 mb-2'>
-      {categories.map((category, index) => (
+      {categories.map((item, index) => (
         <TouchableOpacity
           key={index}
-          className={`flex flex-col items-start px-2.5 py-1.5 m-1 rounded-lg ${selectedCategory === category.title ? 'bg-primary-100' : 'bg-white border border-primary-200'}`}
-          onPress={() => handleCategory(category.title)}
+          className={`flex flex-col items-start px-2.5 py-1.5 m-1 rounded-lg ${
+            selectedCategory === item.category
+              ? 'bg-primary-100'
+              : 'bg-white border border-primary-200'
+          }`}
+          onPress={() => handleCategoryClick(item.category)}
         >
-          <Text className={`text-black text-sm ${selectedCategory === category.title ? 'text-white font-rubik-bold' : 'text-black font-rubik'}`}>{category.title}</Text>
+          <Text
+            className={`text-black text-sm ${
+              selectedCategory === item.category
+                ? 'text-white font-rubik-bold'
+                : 'text-black font-rubik'
+            }`}
+          >
+            {item.title}
+          </Text>
         </TouchableOpacity>
       ))}
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Filters
+export default Filters;
